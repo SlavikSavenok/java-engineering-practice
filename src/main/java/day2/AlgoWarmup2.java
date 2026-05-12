fffcc847d33de4ce53f96729bb8e25add324d914
SlavikSavenok <savenokslava@gmail.com>
2026-05-12 23:37:04 +0300

    implement reverseString with JavaDoc

--- src/main/java/day2/AlgoWarmup2.java
+++ src/main/java/day2/AlgoWarmup2.java
@@ -3,6 +3,7 @@ package day2;
 import java.util.Arrays;
 import java.util.Random;
 
+
 /**
  * Алгоритмическая разминка второго дня.
  * Содержит задачи: сумма массива, реверс строки, фильтрация чётных чисел.
@@ -22,6 +23,9 @@ public class AlgoWarmup2 {
 
         System.out.println("Array: " + Arrays.toString(arr));
         System.out.println("Sum: " + sumArray(arr));
+
+        String string = "AlgoWarmup2";
+        System.out.println(reverseString(string));
     }
 
     /**
@@ -36,5 +40,18 @@ public class AlgoWarmup2 {
             sum += arr[i];
         }
         return sum;
+    }
+
+    /**
+     * Разворачивает строку в обратном порядке.
+     * @param s исходная строка
+     * @return перевёрнутая строка
+     */
+    public static String reverseString(String s) {
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = s.length() - 1; i >= 0; i--) {
+            stringBuilder.append(s.charAt(i));
+        }
+        return stringBuilder.toString();
     }
 }
