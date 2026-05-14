94d04468cd8d510888e2ff2173028f044ba301e0
SlavikSavenok <savenokslava@gmail.com>
2026-05-14 23:11:41 +0300

    algorithm warmup with filterEven

--- src/main/java/day2/AlgoWarmup2.java
+++ src/main/java/day2/AlgoWarmup2.java
@@ -1,5 +1,6 @@
 package day2;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Random;
 
@@ -26,14 +27,24 @@ public class AlgoWarmup2 {
 
         String string = "AlgoWarmup2";
         System.out.println(reverseString(string));
+
+        int listSize = random.nextInt(21);
+
+        ArrayList<Integer> numbers = new ArrayList<>();
+        for (int i = 0; i < listSize; i++) {
+            numbers.add(random.nextInt(101));
+        }
+
+        System.out.println("Original list: " + numbers);
+        System.out.println("Even numbers: " + filterEven(numbers));
     }
 
     /**
      * Суммирует элементы массива.
+     *
      * @param arr массив целых чисел
      * @return сумма элементов
      */
-
     public static int sumArray(int[] arr) {
         int sum = 0;
         for (int i = 0; i < arr.length; i++) {
@@ -44,6 +55,7 @@ public class AlgoWarmup2 {
 
     /**
      * Разворачивает строку в обратном порядке.
+     *
      * @param s исходная строка
      * @return перевёрнутая строка
      */
@@ -54,4 +66,22 @@ public class AlgoWarmup2 {
         }
         return stringBuilder.toString();
     }
+
+    /**
+     * Фильтрует список, оставляя только чётные числа.
+     *
+     * @param list исходный список целых чисел
+     * @return новый список, содержащий только чётные числа
+     */
+    public static ArrayList<Integer> filterEven(ArrayList<Integer> list) {
+        ArrayList<Integer> evenList = new ArrayList<>();
+        for (int num : list) {
+            if (num % 2 == 0) {
+                evenList.add(num);
+            }
+        }
+        return evenList;
+    }
 }
+
+
